84c6b64f0381ee76f75bd8b0acb5581459dedf27
Leonardo <ep.leonardorocha@gmail.com>
2025-08-13 12:14:52 -0300

    36 - Arrays Multidimensionais pt 01

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 21;
+        idades[1] = 13;
+        idades[2] = 2;
+        System.out.println(idades[0]);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula08ArraysMultidimensionais {
+    public static void main(String[] args) {
+        // 1,2,3,4 Meses
+        // 31,28,31,30 Dias
+        int[][] dias = new int[3][3];
+
+    }
+}
