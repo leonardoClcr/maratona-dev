92b19093098a49cb241f58c6fb7dfa88e61d10ea
Leonardo <rochalcr.dev@gmail.com>
2025-08-27 19:10:24 -0300

    BR 88 - Orientação Objetos - Interfaces pt 02 - Implementando múltiplas interfaces

---
+++ src/academy/devdojo/maratonajava/javacore/classesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.classesabstratas.test;
+
+import academy.devdojo.maratonajava.javacore.classesabstratas.dominio.Gerente;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente("Leo", 6250);
+        System.out.println(gerente);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/interfaces/dominio/DataLoader.java
@@ -0,0 +1,5 @@
+package academy.devdojo.maratonajava.javacore.interfaces.dominio;
+
+public interface DataLoader {
+    void load();
+}
